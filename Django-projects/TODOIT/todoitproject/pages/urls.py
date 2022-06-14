98160c4322d91d7aa6c66c54e212a9bb722e7aea
eromanova <eromanova@molnet.ru>
2022-06-14 10:56:47 +0300

    added hw_django_todoit_part_02 results

--- Django-projects/TODOIT/todoitproject/pages/urls.py
+++ Django-projects/TODOIT/todoitproject/pages/urls.py
@@ -4,5 +4,6 @@ from pages import views
 
 urlpatterns = [ 
     path('', views.index, name='index'), 
+    path('edit', views.edit, name='edit'),
     path('random_users', views.hw_users, name='random_users'), 
 ]
--- Django-projects/TODOIT/todoitproject/pages/views.py
+++ Django-projects/TODOIT/todoitproject/pages/views.py
@@ -6,7 +6,9 @@ def index(request):
     context = {} 
     return render(request, 'pages/index.html', context=context)
 
-
+def edit(request): 
+    context = {} 
+    return render(request, 'pages/edit.html', context=context)
 
 BASE_URL = 'https://jsonplaceholder.typicode.com' # Базовый URL
 users_url = '/users' # 10 пользователей
